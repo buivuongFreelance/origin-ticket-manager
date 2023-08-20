67ee8b651f500eb4b374797f93938815e572c8aa
vuong.bui1 <vuong.bui@techvsi.com>
2023-08-20 15:45:14 +0700

    .

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -9,8 +9,18 @@ import { errorHandler } from './middlewares/error-handler';
 import { NotFoundError } from './errors/not-found-error';
 import mongoose from 'mongoose';
 
+import cookieSession from 'cookie-session';
+
 const app = express();
+app.set('trust proxy', true);
+
 app.use(json());
+app.use(
+    cookieSession({
+        signed: false,
+        secure: true,
+    })
+);
 
 app.use(currentUserRouter);
 app.use(signinRouter);
@@ -24,6 +34,10 @@ app.all('*', async (req, res, next) => {
 app.use(errorHandler);
 
 const start = async () => {
+    if(!process.env.JWT_KEY) {
+        throw new Error('JWT_KEY must be defined');
+    }
+
     try {
         await mongoose.connect('mongodb://auth-mongo-srv:27017/auth');
         console.log('Connected to MongoDB');
--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,7 +1,10 @@
 import express, { Request, Response } from 'express';
 import { body, validationResult } from 'express-validator';
 import { RequestValidationError } from '../errors/request-validation-error';
-import { DatabaseConnectionError } from '../errors/database-connection-error';
+import { User } from '../models/user';
+import { BadRequestError } from '../errors/bad-request-error';
+
+import jwt from 'jsonwebtoken';
 
 const router = express.Router();
 
@@ -17,10 +20,25 @@ router.post('/api/users/signup', [
 
     const { email, password } = req.body;
 
-    console.log('Creating a user...');
-    throw new DatabaseConnectionError();
+    const existingUser = await User.findOne({ email });
+    if(existingUser) {
+        throw new BadRequestError('Email in use');
+    }
 
-    res.send({});
+    const user = User.build({ email, password });
+    await user.save();
+
+    const userJwt = jwt.sign({
+        id: user.id,
+        email: user.email
+    }, process.env.JWT_KEY!);
+
+    req.session = {
+        jwt: userJwt
+    }
+
+
+    res.status(201).send(user);
 });
 
 export { router as signupRouter };
